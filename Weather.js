09b7f8fdf187e4431fab6e78d6d50f81ab2f3390
hanum <rntls123@naver.com>
2021-09-05 19:38:51 +0900

    09.05 Get weather condition

--- Weather.js
+++ Weather.js
@@ -12,6 +12,18 @@ export default function Weather({ temp }) {
 };
 Weather.propTypes = {
     temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+    ]).isRequired
 };
 
 const styles = StyleSheet.create({
